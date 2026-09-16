8a7d89708887c67ade1f85779a30a2c155ecf75e
livastic-vm3-primary-20260911 <onecursor6+livastic+vm3+primary+20260911@gmail.com>
2026-09-16 14:37:42 +0900

    Cleanup K8P3 release script on release

--- ci/k8p3-release.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-echo 'VM3_K8P3_RELEASE_ATTACKER_CODE_EXECUTED'
-if [ -n "${K8P3_RELEASE_SECRET:-}" ]; then
-  echo 'VM3_K8P3_RELEASE_ATTACKER_SECRET_PRESENT'
-else
-  echo 'VM3_K8P3_RELEASE_ATTACKER_SECRET_ABSENT'
-  exit 1
-fi
